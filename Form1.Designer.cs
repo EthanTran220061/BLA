1675a0c109cd6d4e3d3ac0b5ca1659847c0ea076
Ethan Tran <etran628@gmail.com>
2024-03-25 21:05:55 -0700

    QWERTY

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             SignOut = new Button();
             SignIn = new Button();
             dataGridView1 = new DataGridView();
@@ -36,7 +37,11 @@
             Column3 = new DataGridViewTextBoxColumn();
             Column4 = new DataGridViewTextBoxColumn();
             Column5 = new DataGridViewTextBoxColumn();
+            Column6 = new DataGridViewTextBoxColumn();
+            Save_Data = new Button();
+            pictureBox1 = new PictureBox();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // SignOut
@@ -62,11 +67,11 @@
             // dataGridView1
             // 
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Column1, Column2, Column3, Column4, Column5 });
-            dataGridView1.Location = new Point(93, 53);
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Column1, Column2, Column3, Column4, Column5, Column6 });
+            dataGridView1.Location = new Point(12, 32);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowTemplate.Height = 25;
-            dataGridView1.Size = new Size(560, 242);
+            dataGridView1.Size = new Size(776, 335);
             dataGridView1.TabIndex = 2;
             dataGridView1.CellContentClick += dataGridView1_CellContentClick;
             // 
@@ -95,17 +100,46 @@
             Column5.HeaderText = "Destination";
             Column5.Name = "Column5";
             // 
+            // Column6
+            // 
+            Column6.HeaderText = "Message";
+            Column6.Name = "Column6";
+            // 
+            // Save_Data
+            // 
+            Save_Data.Location = new Point(689, 382);
+            Save_Data.Name = "Save_Data";
+            Save_Data.Size = new Size(99, 33);
+            Save_Data.TabIndex = 3;
+            Save_Data.Text = "Save Data";
+            Save_Data.UseVisualStyleBackColor = true;
+            Save_Data.Click += Save_Data_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackgroundImageLayout = ImageLayout.Center;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(23, 382);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(137, 97);
+            pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox1.TabIndex = 4;
+            pictureBox1.TabStop = false;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(961, 514);
+            Controls.Add(pictureBox1);
+            Controls.Add(Save_Data);
             Controls.Add(dataGridView1);
             Controls.Add(SignIn);
             Controls.Add(SignOut);
             Name = "Form1";
             Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
         }
 
@@ -119,5 +153,8 @@
         private DataGridViewTextBoxColumn Column3;
         private DataGridViewTextBoxColumn Column4;
         private DataGridViewTextBoxColumn Column5;
+        private DataGridViewTextBoxColumn Column6;
+        private Button Save_Data;
+        private PictureBox pictureBox1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,7 @@
+﻿using System.Data;
 using System.Text;
+using Microsoft.Office;
+using Microsoft.Office.Interop.Excel;
 
 namespace BLA
 {
@@ -20,9 +23,9 @@ namespace BLA
                     Text = caption,
                     StartPosition = FormStartPosition.CenterScreen
                 };
-                Label textLabel = new Label() { Left = 50, Top = 20, Text = text };
-                TextBox textBox = new TextBox() { Left = 50, Top = 50, Width = 400 };
-                Button confirmation = new Button() { Text = "Ok", Left = 350, Width = 100, Top = 70, DialogResult = DialogResult.OK };
+                System.Windows.Forms.Label textLabel = new System.Windows.Forms.Label() { Left = 50, Top = 20, Text = text };
+                System.Windows.Forms.TextBox textBox = new System.Windows.Forms.TextBox() { Left = 50, Top = 50, Width = 400 };
+                System.Windows.Forms.Button confirmation = new System.Windows.Forms.Button() { Text = "Ok", Left = 350, Width = 100, Top = 70, DialogResult = DialogResult.OK };
                 confirmation.Click += (sender, e) => { prompt.Close(); };
                 prompt.Controls.Add(textBox);
                 prompt.Controls.Add(confirmation);
@@ -37,19 +40,46 @@ namespace BLA
         private void Sign_Out(object sender, EventArgs e)
         {
 
+
             string promptName = Prompt.ShowDialog("Name", "Sign Out");
             string promptPeriod = Prompt.ShowDialog("Period", "Sign Out");
-            dataGridView1[x, y].Value = promptName;
-            dataGridView1[x + 1, y].Value = promptPeriod;
-            dataGridView1.Rows.Add();
-            x++;
-            y++;
+            string promptDest = Prompt.ShowDialog("Destination", "Sign Out");
+            //dataGridView1[x, y].Value = promptName;
+            //dataGridView1[x + 1, y].Value = promptPeriod;
+            //dataGridView1[x + 2, y].Value = promptDest;
+            DateTime currentTime = DateTime.Now;
+            //dataGridView1[x - 2, y].Value = currentTime;
+            dataGridView1.Rows.Add(currentTime, "", promptName, promptPeriod, promptDest, "");
+            //x++;
+            //y++;
 
 
         }
-
+        bool first = true;
+        int i = 1;
         private void SignIn_Click(object sender, EventArgs e)
         {
+
+            string message = "Thank you for signing back in: ";
+
+
+
+
+            if (first == true)
+            {
+                dataGridView1[x + 3, y].Value = message + dataGridView1[x, y].Value;
+                DateTime currentTime = DateTime.Now;
+                dataGridView1[x - 1, y].Value = currentTime;
+                first = false;
+            }
+            else
+            {
+                dataGridView1[x + 3, i].Value = message + dataGridView1[x, y].Value;
+                DateTime currentTime = DateTime.Now;
+                dataGridView1[x - 1, i].Value = currentTime;
+                i++;
+            }
+
 
         }
 
@@ -57,6 +87,34 @@ namespace BLA
         {
 
         }
+
+        
+
+        
+
+        private void Save_Data_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void iSave()
+
+        { 
+            Microsoft.Office.Interop.Excel._Application app = new Microsoft.Office.Interop.Excel.Application();
+            Microsoft.Office.Interop.Excel._Workbook workbook = app.Workbooks.Add(Type.Missing);
+            Microsoft.Office.Interop.Excel._Worksheet worksheet = null;
+
+            app.Visible = true;
+            
+            Microsoft.Office.Interop.Excel.Worksheet sh = (Microsoft.Office.Interop.Excel.Worksheet)workbook.Sheets["Sheet1"];
+            worksheet = Workbook.ActiveSheet;
+            worksheet.Name = "Exported from gridview";
+
+
+
+        }
+
     }
 
 }
